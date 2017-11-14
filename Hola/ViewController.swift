9ac251721c40f560a4ac687c51a72bca9f5e184b
Elia <emrahkrkmz1@gmail.com>
2017-11-14 04:31:41 +0300

    added changes on gif and some stuff

--- Hola/ViewController.swift
+++ Hola/ViewController.swift
@@ -16,7 +16,8 @@ class ViewController: UIViewController {
         view.backgroundColor = UIColor.white
         
         
-        testWithImage(.horizontal)
+//        testWithImage(.horizontal)
+        testWithView(.vertical)
     
     }
 
@@ -42,13 +43,40 @@ class ViewController: UIViewController {
         view1.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
         view1.backgroundColor = UIColor.lightGray
         
+        let label1: UILabel = UILabel()
+        label1.textColor = UIColor.white
+        label1.text = "Simple View Label 1"
+        label1.textAlignment = .center
+        label1.font = UIFont.boldSystemFont(ofSize: 18)
+        label1.frame = CGRect(x: 0, y: 0, width: 250, height:50)
+        label1.center = view1.center
+        view1.addSubview(label1)
+        
         let view2: UIView = UIView()
         view2.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
         view2.backgroundColor = UIColor.brown
         
+        let label2: UILabel = UILabel()
+        label2.textColor = UIColor.white
+        label2.text = "Simple View Label 2"
+        label2.textAlignment = .center
+        label2.font = UIFont.boldSystemFont(ofSize: 18)
+        label2.frame = CGRect(x: 0, y: 0, width: 250, height:50)
+        label2.center = view2.center
+        view2.addSubview(label2)
+        
         let view3: UIView = UIView()
         view3.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
         view3.backgroundColor = UIColor.black
+        
+        let label3: UILabel = UILabel()
+        label3.textColor = UIColor.white
+        label3.text = "Simple View Label 3"
+        label3.textAlignment = .center
+        label3.font = UIFont.boldSystemFont(ofSize: 18)
+        label3.frame = CGRect(x: 0, y: 0, width: 250, height:50)
+        label3.center = view3.center
+        view3.addSubview(label3)
         
         let viewArray: [UIView] = [view1,view2,view3]
         
